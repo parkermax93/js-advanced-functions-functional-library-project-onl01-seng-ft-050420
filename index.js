ce212f564b199b2e2c583cdf41a53123db9c0ac3
Parker Williamson <parker.williamson@gmail.com>
2020-08-24 03:01:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection);
+      for (let i = 0; i < newCollection.length; i++)
+        callback (newCollection[i]);
+        return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      if (!(collection instanceof Array))
+      collection = Object.values(collection);
+      
+      const newArray = [];
+      
+      for (let i = 0; i < collection.length; i++)
+        newArray.push(callback(collection[i]));
+        return newArray;
     },
 
-    reduce: function() {
+    reduce: function(c = [], callback = () => {}, acc) {
 
     },
 
